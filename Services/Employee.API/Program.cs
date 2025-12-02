131e07695b596861ed0ee063887a2f9b34b00330
Ilyas <ilyasdabholkar9@gmail.com>
2025-12-03 00:53:51 +0530

    Added CORS configuraions to allow all requests in Employee API

--- Services/Employee.API/Program.cs
+++ Services/Employee.API/Program.cs
@@ -3,6 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -16,6 +26,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
